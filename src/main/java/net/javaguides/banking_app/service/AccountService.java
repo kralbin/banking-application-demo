77ade39f164eaf75ee72f4993f83e8fd091746ab
albin <albinrajank@gmail.com>
2024-10-13 00:01:41 +0530

    13Oct

--- src/main/java/net/javaguides/banking_app/service/AccountService.java
+++ src/main/java/net/javaguides/banking_app/service/AccountService.java
@@ -5,4 +5,8 @@ import net.javaguides.banking_app.dto.AccountDto;
 public interface AccountService {
 
     AccountDto createAccount(AccountDto accountDto);
+
+    AccountDto getAccountById(Long id);
+
+    AccountDto deposit(Long id, double amount);
 }
---
+++ src/main/java/net/javaguides/banking_app/service/impl/AccountServiceImpl.java
@@ -0,0 +1,44 @@
+package net.javaguides.banking_app.service.impl;
+
+import net.javaguides.banking_app.dto.AccountDto;
+import net.javaguides.banking_app.entity.Account;
+import net.javaguides.banking_app.mapper.AccountMapper;
+import net.javaguides.banking_app.repository.AccountRepository;
+import net.javaguides.banking_app.service.AccountService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AccountServiceImpl implements AccountService {
+    public AccountServiceImpl(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+    private AccountRepository accountRepository;
+    @Override
+    public AccountDto createAccount(AccountDto accountDto) {
+        Account account = AccountMapper.mapToAccount(accountDto);
+        Account savedAccount = accountRepository.save(account);
+        return AccountMapper.mapToAccountDto(savedAccount);
+    }
+
+    @Override
+    public AccountDto getAccountById(Long id) {
+        Account account = accountRepository
+                .findById(id)
+                .orElseThrow(()-> new RuntimeException("Account does not exists"));
+        return AccountMapper.mapToAccountDto(account);
+    }
+
+    @Override
+    public AccountDto deposit(Long id, double amount) {
+        Account account = accountRepository
+                .findById(id)
+                .orElseThrow(()-> new RuntimeException("Account does not exists"));
+
+        double total = account.getBalance() + amount;
+        account.setbalance(total);
+        Account savedAccount = accountRepository.save(account);
+
+        return AccountMapper.mapToAccountDto((savedAccount));
+    }
+}
